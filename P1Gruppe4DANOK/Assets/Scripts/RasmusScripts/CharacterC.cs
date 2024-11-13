f0993ab09f2a0fc97c7b29c9ef24716c267657c5
Rasmus <71371162+HauyneX@users.noreply.github.com>
2024-11-13 11:10:51 +0100

    Character V1
    Merge efter min egen fuck up

---
+++ P1Gruppe4DANOK/Assets/Scripts/RasmusScripts/CharacterC.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterC : MonoBehaviour
+{
+    public GameObject[] hats; // Array to hold hats
+    public GameObject[] bottoms; // Array to hold bottoms
+    private int currentHatIndex = 0;
+    private int currentBottomIndex = 0;
+    public static List<string> namesOfHats = new List<string>();
+    public static List<string> namesOfBottoms = new List<string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(hats.Length > 0)
+        {
+            for (int i = 0; i < hats.Length; i++)
+            {
+                hats[i].SetActive(i == currentHatIndex); // Shows the active hat from index
+            }
+        }
+        if(bottoms.Length > 0)
+        {
+            for (int i = 0; i < bottoms.Length; i++)
+            {
+                bottoms[i].SetActive(i == currentBottomIndex); // Shows the active bottom from index
+            }
+        }    
+    }
+
+    public void ChangeHat()
+    {
+        hats[currentHatIndex].SetActive(false); // Deactivate the currently shown hat
+        currentHatIndex = (currentHatIndex + 1) % hats.Length; // Move on to the next hat in the array
+        hats[currentHatIndex].SetActive(true); // Activate the new hat
+    }
+
+    public void ChangeBottom()
+    {
+        bottoms[currentBottomIndex].SetActive(false); // Deactivate the currently shown bottom
+        currentBottomIndex = (currentBottomIndex + 1) % bottoms.Length; // Move on to the next bottom in the array
+        bottoms[currentBottomIndex].SetActive(true); // Activate the new bottom
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
