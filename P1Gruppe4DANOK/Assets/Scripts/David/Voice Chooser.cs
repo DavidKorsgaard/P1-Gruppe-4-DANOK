eb91812f9015eceedcd955af7db32c30dd07f240
DavidKorsgaard <david.korsgaard@gmail.com>
2024-11-19 10:32:05 +0100

    asd

--- P1Gruppe4DANOK/Assets/Scripts/David/Voice Chooser.cs
+++ P1Gruppe4DANOK/Assets/Scripts/David/Voice Chooser.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class VoiceChooser : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public AudioSource 
     void Start()
     {
-        
+        Pu
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
