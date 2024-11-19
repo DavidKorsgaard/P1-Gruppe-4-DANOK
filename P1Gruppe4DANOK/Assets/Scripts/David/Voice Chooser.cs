1f1a0cbd59dadc9969e797840975e35a3fcd6753
Rasmus <71371162+HauyneX@users.noreply.github.com>
2024-11-19 13:39:24 +0100

    V1 Character customization

--- P1Gruppe4DANOK/Assets/Scripts/David/Voice Chooser.cs
+++ P1Gruppe4DANOK/Assets/Scripts/David/Voice Chooser.cs
@@ -2,13 +2,4 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VoiceChooser : MonoBehaviour
-{
-    public AudioSource 
-    void Start()
-    {
-        Pu
-    }
 
-    
-}
