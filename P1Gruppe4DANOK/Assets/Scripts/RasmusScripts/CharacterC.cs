316af4309eb6e5d9d703fddf442d8631509e74c5
DavidKorsgaard <david.korsgaard@gmail.com>
2024-11-19 10:44:27 +0100

    Revert "Character Custom v2"
    This reverts commit d18a8d2c97f3681fcedf641ee241d57c5a9aac6c.

--- P1Gruppe4DANOK/Assets/Scripts/RasmusScripts/CharacterC.cs
+++ P1Gruppe4DANOK/Assets/Scripts/RasmusScripts/CharacterC.cs
@@ -26,31 +26,20 @@ public class CharacterC : MonoBehaviour
             for (int i = 0; i < bottoms.Length; i++)
             {
                 bottoms[i].SetActive(i == currentBottomIndex); // Shows the active bottom from index
-                
             }
         }    
     }
 
-    public void OnChangeHatButtonClick()
-    {
-            ChangeHat();
-    }
-
-    public void OnChangeBottomButtonClick()
-    {
-            ChangeBottom();
-    }
-
     public void ChangeHat()
     {
-        hats[currentHatIndex].SetActive(false); // Deactivate the current hat
+        hats[currentHatIndex].SetActive(false); // Deactivate the currently shown hat
         currentHatIndex = (currentHatIndex + 1) % hats.Length; // Move on to the next hat in the array
         hats[currentHatIndex].SetActive(true); // Activate the new hat
     }
 
     public void ChangeBottom()
     {
-        bottoms[currentBottomIndex].SetActive(false); // Deactivate the current bottom
+        bottoms[currentBottomIndex].SetActive(false); // Deactivate the currently shown bottom
         currentBottomIndex = (currentBottomIndex + 1) % bottoms.Length; // Move on to the next bottom in the array
         bottoms[currentBottomIndex].SetActive(true); // Activate the new bottom
     }
