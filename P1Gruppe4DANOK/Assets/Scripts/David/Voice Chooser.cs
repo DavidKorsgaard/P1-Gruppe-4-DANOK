b46a95d33193d067c72370a070924a3670a64d39
Rasmus <71371162+HauyneX@users.noreply.github.com>
2024-11-19 13:42:31 +0100

    Update Voice Chooser.cs

--- P1Gruppe4DANOK/Assets/Scripts/David/Voice Chooser.cs
+++ P1Gruppe4DANOK/Assets/Scripts/David/Voice Chooser.cs
@@ -2,4 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+<<<<<<< HEAD
 
+=======
+public class VoiceChooser : MonoBehaviour
+{
+    public AudioSource thisPlaying;
+    public bool isPlaying;
+    public AudioClip[] myClips; // Skiftet fra ArrayList[] til AudioClip[]
+
+    private void Start()
+    {
+        // Loader alle AudioClip-filer fra mappen "Resources/audio/foo"
+        myClips = Resources.LoadAll<AudioClip>("audio/foo");
+
+        // Debugging: Tjek om klippene er blevet loadet korrekt
+        foreach (var clip in myClips)
+        {
+            Debug.Log("Loaded AudioClip: " + clip.name);
+        }
+    }
+}
+>>>>>>> 8c73d8dff5fcd1687f6eb2ecad50f4e2e60e6584
