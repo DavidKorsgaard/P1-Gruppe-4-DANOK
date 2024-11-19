8c73d8dff5fcd1687f6eb2ecad50f4e2e60e6584
DavidKorsgaard <david.korsgaard@gmail.com>
2024-11-19 13:22:30 +0100

    Some more Audio change

--- P1Gruppe4DANOK/Assets/Scripts/David/Voice Chooser.cs
+++ P1Gruppe4DANOK/Assets/Scripts/David/Voice Chooser.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class VoiceChooser : MonoBehaviour
 {
-    public AudioSource 
-    void Start()
-    {
-        Pu
-    }
+    public AudioSource thisPlaying;
+    public bool isPlaying;
+    public AudioClip[] myClips; // Skiftet fra ArrayList[] til AudioClip[]
 
-    
+    private void Start()
+    {
+        // Loader alle AudioClip-filer fra mappen "Resources/audio/foo"
+        myClips = Resources.LoadAll<AudioClip>("audio/foo");
+
+        // Debugging: Tjek om klippene er blevet loadet korrekt
+        foreach (var clip in myClips)
+        {
+            Debug.Log("Loaded AudioClip: " + clip.name);
+        }
+    }
 }
